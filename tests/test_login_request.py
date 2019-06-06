3cd10d96f23283d83916d1df857f22683c92845b
Aliya <ktotamy@gmail.com>
2019-06-06 12:48:36 +0300

    test login(request)

---
+++ tests/test_login_request.py
@@ -0,0 +1,69 @@
+import requests
+import pytest
+import string
+import random
+
+
+url = 'http://localhost:8080/j_acegi_security_check'
+headers = {
+    'content-type': 'application/x-www-form-urlencoded'
+}
+
+
+# def random_string(string_length=10):
+#     letters = string.ascii_lowercase
+#     return ''.join(random.choice(letters) for _ in range(string_length))
+
+
+def create_data(j_username, j_password, from_input='/'):
+    data = {
+        'j_username': j_username,
+        'j_password': j_password,
+        'from': from_input,
+        'Submit': 'Sign in'
+    }
+    print(data)
+    print()
+    return data
+
+
+def test_login_success():
+    data = create_data("User1", "1234")
+
+    response = requests.post(url, params=data, headers=headers)
+    print(response.text)
+
+    assert response.status_code == 200
+    assert response.headers['Content-Type'] == 'text/html;charset=utf-8'
+    assert 'log out' in response.text
+
+
+def test_invalid_username():
+    data = create_data("userrrrr", "1234")
+
+    response = requests.post(url, params=data, headers=headers)
+
+    assert response.status_code == 401
+    assert response.headers['Content-Type'] == 'text/html;charset=utf-8'
+    assert 'Invalid username or password' in response.text
+
+
+def test_invalid_password():
+    data = create_data("User1", "123456")
+
+    response = requests.post(url, params=data, headers=headers)
+
+    assert response.status_code == 401
+    assert response.headers['Content-Type'] == 'text/html;charset=utf-8'
+    assert 'Invalid username or password' in response.text
+
+
+def test_invalid_from():
+    data = create_data('User1', '1234', '/++++')
+
+    response = requests.post(url, params=data, headers=headers)
+    print(response.text)
+
+    assert response.status_code == 404
+    assert response.headers['Content-Type'] == 'text/html;charset=iso-8859-1'
+    assert 'Not Found' in response.text
